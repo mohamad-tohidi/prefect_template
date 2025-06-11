5a8011fac3fba3800b2dbeca46ab0a3d5fde6dba
Tohidi <the.mohammad.tohidi@gmail.com>
2025-06-11 12:00:53 +0330

    feat: adding ES configs to .env

--- deploy.py
+++ deploy.py
@@ -2,7 +2,10 @@
 Run once (or per commit) to register / update the deployment.
 """
 from reindex import reindex
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 if __name__ == "__main__":
     reindex.from_source(
         source="https://github.com/mohamad-tohidi/prefect_tutorial.git",
@@ -12,4 +15,9 @@ if __name__ == "__main__":
         work_pool_name="etl",
         parameters={"max_slices": 8},
         tags=["elasticsearch-etl"],
+        env={
+            "ES_URL":  os.getenv("ES_URL"),
+            "ES_USER": os.getenv("ES_USER"),
+            "ES_PASS": os.getenv("ES_PASS"),
+        },
     )
